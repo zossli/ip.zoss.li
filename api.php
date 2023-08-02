b790b3426437bfb6d6b30f673403f424c2e75159
Reto Zoss <zossli@users.noreply.github.com>
2023-08-02 18:24:10 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,17 +1,3 @@
-<HTML>
-
-<HEAD>
-<TITLE>ip.zoss.li - IP Infos</TITLE>
-<meta name="description=" content="&Uuml;berpr&uuml;fe deine public und local used IP Adresse. Zudem siehst du hilfreiche Informationen aus der RIPE DB.">
-<link rel="icon" href="data:;base64,iVBORw0KGgo=">
-</HEAD>
-<BODY BGCOLOR="#ffffff" TEXT="#000000" LINK="#0000a0" VLINK="#0000a0" ALINK="#0000a0">
-
 <?php
-
 echo $_SERVER['REMOTE_ADDR'];
-
 ?>
-</BODY>
-
-</HTML>
